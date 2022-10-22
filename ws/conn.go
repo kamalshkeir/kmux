6b5c071dbf3eb6f88a9767105bf0b5778878c515
kamalshkeir <kamalshkeir@yahoo.fr>
2022-10-22 17:38:47 +0200

    handle concurrent ws writing

--- ws/conn.go
+++ ws/conn.go
@@ -723,19 +723,15 @@ func (w *messageWriter) ReadFrom(r io.Reader) (nn int64, err error) {
 	return nn, err
 }
 
-var messageWriterMU sync.Mutex
+
 func (w *messageWriter) Close() error {
-	messageWriterMU.Lock()
 	if w.err != nil {
-		messageWriterMU.Unlock()
 		return w.err
 	}
 	err := w.flushFrame(true, nil)
 	if err != nil {
-		messageWriterMU.Unlock()
 		return err
 	}
-	messageWriterMU.Unlock()
 	return nil
 }
 
