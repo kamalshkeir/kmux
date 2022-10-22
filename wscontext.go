4a83d058df43ed4d4b16c245f0f646a529720f77
kamalshkeir <kamalshkeir@yahoo.fr>
2022-10-22 15:43:55 +0200

    handle concurrent ws

--- wscontext.go
+++ wscontext.go
@@ -6,86 +6,97 @@ import (
 	"github.com/kamalshkeir/kmux/ws"
 )
 
-var m sync.RWMutex
 
 type WsContext struct {
 	Ws     *ws.Conn
 	Params map[string]string
 	Route
+	mu     sync.RWMutex
 }
 
 // ReceiveText receive text from ws and disconnect when stop receiving
 func (c *WsContext) ReceiveText() (string, error) {
+	c.mu.RLock()
 	_,msgByte,err := c.Ws.ReadMessage()
 	if err != nil {
+		c.mu.Unlock()
 		return "", err
 	}
+	c.mu.RUnlock()
 	return string(msgByte), nil
 }
-
 
 // ReceiveJson receive json from ws and disconnect when stop receiving
 func (c *WsContext) ReceiveJson() (map[string]any, error) {
 	var data map[string]any
+	c.mu.RLock()
 	err := c.Ws.ReadJSON(&data)
 	if err != nil {
+		c.mu.RUnlock()
 		return nil, err
 	}
+	c.mu.RUnlock()
 	return data, nil
 }
 
 // Json send json to the client
 func (c *WsContext) Json(data map[string]any) error {
-	return c.Ws.WriteJSON(data)
-	// err := websocket.JSON.Send(c.Ws, data)
-	// if err != nil {
-	// 	return err
-	// }
-	// return nil
+	c.mu.Lock()
+	err := c.Ws.WriteJSON(data)
+	if err != nil {
+		c.mu.Unlock()
+		return err
+	}
+	c.mu.Unlock()
+	return nil
 }
 
 // Broadcast send message to all clients in c.Clients
 func (c *WsContext) Broadcast(data any) error {
-	m.RLock()
+	c.mu.Lock()
 	for _, ws := range c.Route.Clients {
 		err := ws.WriteJSON(data)
 		if err != nil {
-			m.RUnlock()
+			c.mu.Unlock()
 			return err
 		}
 	}
-	m.RUnlock()
+	c.mu.Unlock()
 	return nil
 }
 
 // BroadcastExceptCaller send message to all clients in c.Clients
 func (c *WsContext) BroadcastExceptCaller(data map[string]any) error {
-	m.RLock()
+	c.mu.Lock()
 	for _, ws := range c.Route.Clients {
 		if ws != c.Ws {
 			err := ws.WriteJSON(data)
 			if err != nil {
-				m.RUnlock()
+				c.mu.Unlock()
 				return err
 			}
 		}
 	}
-	m.RUnlock()
+	c.mu.Unlock()
 	return nil
 }
 
 // Text send text to the client
 func (c *WsContext) Text(data string) error {
+	c.mu.Lock()
 	err := c.Ws.WriteMessage(ws.BinaryMessage,[]byte(data))
 	if err != nil {
+		c.mu.Unlock()
 		return err
 	}
+	c.mu.Unlock()
 	return nil
 }
 
 // RemoveRequester remove the client from Clients list in context
 func (c *WsContext) RemoveRequester(name ...string) {
-	m.Lock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	for k, ws := range c.Route.Clients {
 		if len(name) > 1 {
 			n := name[0]
@@ -104,12 +115,12 @@ func (c *WsContext) RemoveRequester(name ...string) {
 		}
 
 	}
-	m.Unlock()
 }
 
 // AddClient add client to clients_list
 func (c *WsContext) AddClient(key string) {
-	m.Lock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	if _, ok := c.Route.Clients[key]; !ok {
 		c.Route.Clients[key] = c.Ws
 	} else {
@@ -119,5 +130,5 @@ func (c *WsContext) AddClient(key string) {
 			}
 		}
 	}
-	m.Unlock()
+	
 }
