8894d8d98bd391463e2bb28aba8a763b2b0b1d92
kamalshkeir <kamalshkeir@yahoo.fr>
2022-11-02 23:53:09 +0100

    f

--- ws/conn.go
+++ ws/conn.go
@@ -1135,18 +1135,7 @@ func (c *Conn) ReadMessage() (messageType int, p []byte, err error) {
 }
 
 func (c *Conn) Read(p []byte) (n int, err error) {
-	var r io.Reader
-	_, r, err = c.NextReader()
-	if err != nil {
-		if errors.Is(err,errUnexpectedEOF) || errors.Is(err,io.EOF) {
-			c.reader.Close()
-			return  0, nil
-		} else {
-			return  0, err
-		}
-	}
-	
-	n,err = r.Read(p)
+	c.reader.Read(p)
 	if err != nil {
 		if errors.Is(err,errUnexpectedEOF) || errors.Is(err,io.EOF) {
 			c.reader.Close()
